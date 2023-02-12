578a8a093a93d360fd591e8d3052c9d18b73e004
Brian Tyler <brian@hackf5.io>
2023-02-12 09:07:43 +0000

    feat: add sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,20 @@
 const eleventySass = require("eleventy-sass");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 const configure = require("./src/_includes/configure");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://echelon.fail",
+    },
+  });
+
   eleventyConfig.addPassthroughCopy("src/assets/favicon");
   eleventyConfig.addPassthroughCopy("src/assets/js");
 
